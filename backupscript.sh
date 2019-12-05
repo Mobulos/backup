bb4e7419b0b1a045430a8e488285f6ba8f9f4a1a
Mobulos <Fabian.schmeltzer77@outlook.de>
2019-12-05 21:43:43 +0100

    Bug Fixes

--- backupscript.sh
+++ backupscript.sh
@@ -47,8 +47,8 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 
-function print_my_input(backup) {
-  echo 'Backup Script startet ' $backup
+function print_my_input() {
+  echo 'Backup Script startet' $backup
 }
 
 
