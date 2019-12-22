ff2b8f42fbb3c3cde231451541a6966b63c2e776
Mobulos <Fabian.schmeltzer77@outlook.de>
2019-12-22 13:32:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-curl --progress-bar https://raw.githubusercontent.com/Mobulos/backup/master/backupscript.sh --output backupscript.
+curl --progress-bar https://raw.githubusercontent.com/Mobulos/backup/master/backupscript.sh --output backupscript.sh
 read -t 1
 chmod +x backupscript.sh
 ./backupscript.sh
